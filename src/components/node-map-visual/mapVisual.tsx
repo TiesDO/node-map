a3d5ffb4e98c79933b9cea8d83ee353d4d71e2d1
TiesDO <denouden.ties@gmail.com>
2022-12-08 12:10:07 +0100

    Clean slate
    remove boiler plate project code
    create mapVisual component
    render in test view (root.tsx)

---
+++ src/components/node-map-visual/mapVisual.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik';
+
+export type MapVisualType = {
+	gridSize: number;
+};
+
+export const MapVisual = component$((props: MapVisualType) => {
+	return <div></div>;
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { Logo } from './components/logo/logo';
 export { Counter } from './components/counter/counter';
+export { MapVisual } from './components/node-map-visual/mapVisual';
