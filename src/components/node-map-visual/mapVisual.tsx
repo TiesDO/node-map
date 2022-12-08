182a0a970576e98c4534b84f6c851999d3f1744e
TiesDO <denouden.ties@gmail.com>
2022-12-08 16:22:22 +0100

    apply styles reactivly to nodemap

--- src/components/node-map-visual/mapVisual.tsx
+++ src/components/node-map-visual/mapVisual.tsx
@@ -1,12 +1,42 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
+import { component$, useStyles$, useStore, useWatch$ } from '@builder.io/qwik';
 import mainStyle from './nodemap.css?inline';
 
-export type MapVisualType = {
+export type NodeMapSettings = {
 	gridSize: number;
+	mapBackground: string;
+	gridColor: string;
 };
 
-export const MapVisual = component$((props: MapVisualType) => {
+export const NodeMapSettingsDefault: NodeMapSettings = {
+	gridSize: 20,
+	mapBackground: '#333',
+	gridColor: '#555',
+};
+
+export const MapVisual = component$(() => {
 	useStyles$(mainStyle);
 
-	return <div class='nodemap-container'></div>;
+	// set store to default
+	const gridSettings = useStore<NodeMapSettings>(NodeMapSettingsDefault);
+
+	useWatch$(async ({ track }) => {
+		track(() => gridSettings.gridSize);
+	});
+
+	return (
+		<div
+			class='nodemap-container'
+			style={{
+				'--bgc': gridSettings.mapBackground,
+				'--gc': gridSettings.gridColor,
+				'--gs': `${gridSettings.gridSize}px`,
+			}}>
+			<button
+				onClick$={() => {
+					gridSettings.gridSize = gridSettings.gridSize + 5;
+				}}>
+				test
+			</button>
+		</div>
+	);
 });
