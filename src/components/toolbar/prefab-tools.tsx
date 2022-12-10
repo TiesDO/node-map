09f1e20c335ee4e1f8ba87e536058922ee0e2380
TiesDO <denouden.ties@gmail.com>
2022-12-10 12:55:37 +0100

    prefab tool components for create and move

---
+++ src/components/toolbar/prefab-tools.tsx
@@ -0,0 +1,5 @@
+import { ToolbarTool } from './toolbar';
+
+export const MoveTool = <ToolbarTool name='move' q:slot='tools' />;
+
+export const CreateTool = <ToolbarTool name='create' q:slot='tools' />;
