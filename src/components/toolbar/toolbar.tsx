711d3866b98dff736d135ce31dc09f089a85eb8b
TiesDO <denouden.ties@gmail.com>
2022-12-10 12:55:12 +0100

    basic toolbar
    add toolbar comp and styles
    add dragpoint
    add tool component
    add toolbar slot for tools

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,9 +1,40 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { component$, Slot, useStyles$, useStore } from '@builder.io/qwik';
 
 import toolbarStyle from './toolbar.css?inline';
 
-export const Toolbar = component$(() => {
-	useStylesScoped$(toolbarStyle);
+export type ToolbarProps = {};
 
-	return <div class='toolbar'>toolbar</div>;
+export type ToolbarState = {
+	x: number;
+	y: number;
+};
+
+export const Toolbar = component$(() => {
+	useStyles$(toolbarStyle);
+
+	const state = useStore({ x: 15, y: 15 });
+
+	return (
+		<div class='toolbar' style={`--px: ${state.x}px; --py: ${state.y}px`}>
+			<div class='dragpoint'></div>
+			<div class='tools'>
+				<Slot name='tools' />
+			</div>
+		</div>
+	);
+});
+
+export type ToobarToolProps = {
+	name: string;
+	onMouseDown?: () => void;
+	onMouseUp?: () => void;
+	onDragStart?: () => void;
+	onDragMove?: () => void;
+	onDragEnd?: () => void;
+};
+
+export const ToolbarTool = component$((props: ToobarToolProps) => {
+	const name: string = props.name[0] + props.name[1];
+
+	return <div class='tool'>{name.toUpperCase()}</div>;
 });
