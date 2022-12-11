116f4c78aaa8176760efaa05edd69b19eee15224
TiesDO <denouden.ties@gmail.com>
2022-12-11 12:51:58 +0100

    show all props in property panel

--- src/components/node-map-visual/NodePropertiesEditor.tsx
+++ src/components/node-map-visual/NodePropertiesEditor.tsx
@@ -1,30 +1,92 @@
-import { component$, useClientEffect$, useContext } from '@builder.io/qwik';
+import {
+	component$,
+	useClientEffect$,
+	useContext,
+	useStore,
+} from '@builder.io/qwik';
 import { NodeMapContext, NodeMapState } from './NodeMap';
+
+export type NodePropertiesEditorState = {
+	activeNode: any;
+	properties: NodePropertyEditProps[];
+};
 
 export const NodePropertiesEditor = component$(() => {
 	const mapContext = useContext<NodeMapState>(NodeMapContext);
+	// const renderProps = $((id: string) => {
+	// 	// find the right node
+	// 	const nodeProps = mapContext.nodes.find((n) => n.id === id);
+	// 	const panel = document.querySelector('#propertyPanel');
+	// 	if (!panel || !nodeProps) {
+	// 		return;
+	// 	}
+	// 	panel.textContent = nodeProps.text;
+	// });
+	// 	useClientEffect$(({ track }) => {
+	// 		const displayTarget = track(() => mapContext.singleSelect);
+	// 		const panel = document.querySelector('#propertyPanel');
+	// 		if (!panel) {
+	// 			return;
+	// 		}
+	// 		if (displayTarget?.length === 0 || displayTarget === null) {
+	// 			panel.textContent = 'no props to display';
+	// 		} else {
+	// 			renderProps(displayTarget);
+	// 		}
+	// 	});
+
+	// find the current node
+	const state = useStore<NodePropertiesEditorState>({
+		activeNode: null,
+		properties: [],
+	});
 
 	useClientEffect$(({ track }) => {
-		const displayTarget = track(() => mapContext.singleSelect);
+		const targetId = track(() => mapContext.singleSelect);
 
-		console.log(displayTarget);
-
-		const panel = document.querySelector('#propertyPanel');
-
-		if (!panel) {
+		if (!targetId) {
+			state.activeNode = null;
 			return;
 		}
 
-		if (displayTarget?.length === 0 || displayTarget === null) {
-			panel.textContent = 'no props to display';
+		// find the node
+		const node = mapContext.nodes.find((n) => n.id === targetId);
+
+		if (!node) {
+			state.activeNode = null;
+			return;
 		}
 
-		panel.textContent = displayTarget;
+		state.activeNode = node;
+
+		const props: NodePropertyEditProps[] = [];
+
+		for (const [key] of Object.entries(node)) {
+			props.push({
+				propname: key,
+				state: state.activeNode,
+			});
+		}
+
+		state.properties = props;
 	});
 
 	return (
 		<div class='nodemap-properties-editor' id='propertyPanel'>
-			no props to display
+			{state.activeNode
+				? state.properties.map((p) => (
+						<NodePropertyEdit key={p.propname} {...p} />
+				  ))
+				: 'no node selected'}
 		</div>
 	);
+});
+
+export type NodePropertyEditProps = {
+	propname: string;
+	state: any;
+};
+
+export const NodePropertyEdit = component$((props: NodePropertyEditProps) => {
+	return <input value={props.state[props.propname]} />;
 });
