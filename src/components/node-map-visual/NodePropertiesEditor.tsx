49064b93a78282ce720e6b6b346fed240ce68420
TiesDO <denouden.ties@gmail.com>
2022-12-11 10:03:07 +0100

    access state in nodepropertyeditor

--- src/components/node-map-visual/NodePropertiesEditor.tsx
+++ src/components/node-map-visual/NodePropertiesEditor.tsx
@@ -1,5 +1,12 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useContext } from '@builder.io/qwik';
+import { NodeMapContext, NodeMapState } from './NodeMap';
 
 export const NodePropertiesEditor = component$(() => {
+	const { selection } = useContext<NodeMapState>(NodeMapContext);
+
+	if (selection.length < 1) {
+		// show the properties for
+	}
+
 	return <div class='nodemap-properties-editor'></div>;
 });
