1c86099f9eab3ae6b869d6e468d511d6808d297f
TiesDO <denouden.ties@gmail.com>
2022-12-11 10:52:37 +0100

    dragstart threshhold
    add check to start drag logic only after mouse moved a little

--- src/components/toolbar/prefab-tools.tsx
+++ src/components/toolbar/prefab-tools.tsx
@@ -9,30 +9,61 @@ import { v4 } from 'uuid';
 export const MoveTool: ToobarToolProps = {
 	name: 'move',
 	onMouseDown$: $((e: QwikMouseEvent) => {
+		//#region start
+
+		const dragStartThreshold: number = 5;
+
 		const element = e.target as HTMLElement;
 
-		// check if it is a node
+		// check if event target is a node
 		if (!element.classList.contains('node')) {
 			return;
 		}
 
-		const container = element.closest<HTMLElement>('.nodemap-container');
+		// find the canvas
 		const canvas = element.closest<HTMLElement>('.nodemap-canvas');
 
-		if (container === null || canvas === null) {
+		if (canvas === null || canvas === null) {
 			return;
 		}
 
-		const nodeOffset = getElementRelativePosition(e, element);
-		window.onmouseup = () => {
-			container.onmousemove = null;
+		//#endregion
+
+		//#region Dragging logic
+
+		const startDrag = () => {
+			const nodeOffset = getElementRelativePosition(e, element);
+			window.onmouseup = () => {
+				canvas.onmousemove = null;
+			};
+
+			canvas.onmousemove = (ev: MouseEvent) => {
+				const nextPosition = getElementRelativePosition(ev, canvas, nodeOffset);
+
+				element.style.left = nextPosition.x + 'px';
+				element.style.top = nextPosition.y + 'px';
+			};
 		};
 
-		container.onmousemove = (ev: MouseEvent) => {
-			const nextPosition = getElementRelativePosition(ev, canvas, nodeOffset);
+		//#endregion
 
-			element.style.left = nextPosition.x + 'px';
-			element.style.top = nextPosition.y + 'px';
+		// check if the user moves
+		const initialClickPos = { x: e.clientX, y: e.clientY };
+
+		element.onmousemove = (ev: MouseEvent) => {
+			const delta = {
+				x: Math.abs(initialClickPos.x - ev.clientX),
+				y: Math.abs(initialClickPos.y - ev.clientY),
+			};
+
+			if (delta.x + delta.y > dragStartThreshold) {
+				startDrag();
+				element.onmousemove = null;
+			}
+		};
+
+		element.onmouseup = () => {
+			element.onmousemove = null;
 		};
 	}),
 };
