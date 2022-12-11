416bdc780826edcca0c07d41d9fb322956f24e4a
TiesDO <denouden.ties@gmail.com>
2022-12-11 11:57:21 +0100

    panel display node id
    panel displays active node id
    client effect for node swapping

--- src/components/node-map-visual/NodePropertiesEditor.tsx
+++ src/components/node-map-visual/NodePropertiesEditor.tsx
@@ -1,12 +1,30 @@
-import { component$, useContext } from '@builder.io/qwik';
+import { component$, useClientEffect$, useContext } from '@builder.io/qwik';
 import { NodeMapContext, NodeMapState } from './NodeMap';
 
 export const NodePropertiesEditor = component$(() => {
-	const { selection } = useContext<NodeMapState>(NodeMapContext);
+	const mapContext = useContext<NodeMapState>(NodeMapContext);
 
-	if (selection.length < 1) {
-		// show the properties for
-	}
+	useClientEffect$(({ track }) => {
+		const displayTarget = track(() => mapContext.singleSelect);
 
-	return <div class='nodemap-properties-editor'></div>;
+		console.log(displayTarget);
+
+		const panel = document.querySelector('#propertyPanel');
+
+		if (!panel) {
+			return;
+		}
+
+		if (displayTarget?.length === 0 || displayTarget === null) {
+			panel.textContent = 'no props to display';
+		}
+
+		panel.textContent = displayTarget;
+	});
+
+	return (
+		<div class='nodemap-properties-editor' id='propertyPanel'>
+			no props to display
+		</div>
+	);
 });
